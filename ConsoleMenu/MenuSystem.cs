5c98e0812246b5bfa725e97dcdd4a58e26a69ba3
Russell Hoyer <sixstringtrebleterror@gmail.com>
2016-02-20 13:05:24 -0500

    Initial commit... started a few of the classes/methods but nothing more than constructors and such

---
+++ ConsoleMenu/MenuSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMenu
+{
+    class MenuSystem
+    {
+        private string _greeting;
+        private string[] _menuItems;
+
+        public string Greeting
+        {
+            get { return _greeting; }
+            set { _greeting = value; }
+        }
+        public string[] MenuItems
+        {
+            get { return _menuItems; }
+            set { _menuItems = value; }
+        }
+
+        public MenuSystem()
+        {
+        }
+        public MenuSystem(string greeting, string[] menuItems)
+        {
+            _greeting = greeting;
+            _menuItems = menuItems;
+        }
+    }
+}
---
+++ ConsoleMenu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine("Welcome to the menu system!");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
