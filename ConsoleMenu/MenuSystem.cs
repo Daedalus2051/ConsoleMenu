eae9cd1f1631966f3804f118a87441a1e1438857
Russell Hoyer <sixstringtrebleterror@gmail.com>
2016-02-20 14:09:08 -0500

    Added the basic methods/properties needed for initial testing Added the code in the main Program class to run through the menu

--- ConsoleMenu/MenuSystem.cs
+++ ConsoleMenu/MenuSystem.cs
@@ -8,7 +8,9 @@ namespace ConsoleMenu
 {
     class MenuSystem
     {
+        #region Variables
         private string _greeting;
+        private string _prompt;
         private string[] _menuItems;
 
         public string Greeting
@@ -16,12 +18,18 @@ namespace ConsoleMenu
             get { return _greeting; }
             set { _greeting = value; }
         }
+        public string Prompt
+        {
+            get { return _prompt; }
+            set { _prompt = value; }
+        }
         public string[] MenuItems
         {
             get { return _menuItems; }
             set { _menuItems = value; }
         }
-
+        #endregion
+        #region Constructors
         public MenuSystem()
         {
         }
@@ -30,5 +38,47 @@ namespace ConsoleMenu
             _greeting = greeting;
             _menuItems = menuItems;
         }
+        public MenuSystem(string greeting, string prompt, string[] menuItems)
+        {
+            _greeting = greeting;
+            _menuItems = menuItems;
+            _prompt = prompt;
+        }
+        #endregion
+        #region Methods
+        // Load methods; for pulling in menu items from a file
+
+        // Command processor? Probably not generic enough; this class should return the input to be processed by another class for running the methods
+
+        // Draw menu
+        public void DrawMenu()
+        {
+            // Check that the array contains items 
+            if (_menuItems == null)
+            {
+                Console.WriteLine("Error! Menu array contains no items!");
+                return;
+            }
+
+            // Array is valid, run through printing the menu
+            Console.Clear();
+            Console.WriteLine(_greeting);
+            for (int i = 0; i < _menuItems.Length; i++)
+            {
+                Console.WriteLine("{0}. {1}", (i+1), _menuItems[i]);
+            }
+            Console.WriteLine();
+            Console.Write(_prompt);
+        }
+
+        // Receive input (menu item number), returns to caller
+        public int ReadInput()
+        {
+            // The most basic of input... this is mostly just so I can start testing; should change this to be more robust and validate input
+            int input = -1;
+            input = Convert.ToInt32(Console.ReadLine());
+            return input;
+        }
+        #endregion
     }
 }
--- ConsoleMenu/Program.cs
+++ ConsoleMenu/Program.cs
@@ -10,18 +10,55 @@ namespace ConsoleMenu
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Initializing...");
+
             Run();
         }
 
         public static void Run()
         {
             bool isRunning = true;
+            int choice = -1;
+
+            MenuSystem menu = new MenuSystem();
+            menu.Greeting = "Welcome to the ConsoleMenu application!";
+            string[] myMenu = new string[] { "Menu item 1", "Menu item 2", "Menu item 3", "Quit" };
+            menu.MenuItems = myMenu;
+            menu.Prompt = "Choice->";
 
             while (isRunning)
             {
-                Console.WriteLine("Welcome to the menu system!");
-                Console.WriteLine();
-                Console.WriteLine();
+                menu.DrawMenu();
+                choice = menu.ReadInput();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Choice made: 1 yay!");
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Choice made: 2 @choicemade");
+                        Console.ReadLine();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Choice made: 3 #menusystemsareawesome");
+                        Console.ReadLine();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Choice made: 4...Quitting...:(");
+                        Console.ReadLine();
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Command not recognized.");
+                        Console.ReadLine();
+                        break;
+                }
             }
         }
     }
