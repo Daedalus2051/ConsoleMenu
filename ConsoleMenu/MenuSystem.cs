a1ae6b328db26ed3d4af1fa21d48683d01d7aaa5
Russell Hoyer <sixstringtrebleterror@gmail.com>
2016-02-20 14:41:30 -0500

    Added summaries for all of the methods in the MenuSystem class

--- ConsoleMenu/MenuSystem.cs
+++ ConsoleMenu/MenuSystem.cs
@@ -13,16 +13,25 @@ namespace ConsoleMenu
         private string _prompt;
         private string[] _menuItems;
 
+        /// <summary>
+        /// String that will be displayed above the menu.
+        /// </summary>
         public string Greeting
         {
             get { return _greeting; }
             set { _greeting = value; }
         }
+        /// <summary>
+        /// String that will be displayed as the prompt for user input.
+        /// </summary>
         public string Prompt
         {
             get { return _prompt; }
             set { _prompt = value; }
         }
+        /// <summary>
+        /// String array of menu items that will be displayed.
+        /// </summary>
         public string[] MenuItems
         {
             get { return _menuItems; }
@@ -30,14 +39,28 @@ namespace ConsoleMenu
         }
         #endregion
         #region Constructors
+        /// <summary>
+        /// Basic constructor for the MenuSystem class, initializes with no properties; they must be set later.
+        /// </summary>
         public MenuSystem()
         {
         }
+        /// <summary>
+        /// Basic constructor for the MenuSystem class
+        /// </summary>
+        /// <param name="greeting">String that will be displayed above the menu.</param>
+        /// <param name="menuItems">String array of menu items that will be displayed.</param>
         public MenuSystem(string greeting, string[] menuItems)
         {
             _greeting = greeting;
             _menuItems = menuItems;
         }
+        /// <summary>
+        /// Basic constructor for the MenuSystem class.
+        /// </summary>
+        /// <param name="greeting">String that will be displayed above the menu.</param>
+        /// <param name="prompt">String that will be displayed as the prompt for user input.</param>
+        /// <param name="menuItems">String array of menu items that will be displayed.</param>
         public MenuSystem(string greeting, string prompt, string[] menuItems)
         {
             _greeting = greeting;
@@ -51,6 +74,9 @@ namespace ConsoleMenu
         // Command processor? Probably not generic enough; this class should return the input to be processed by another class for running the methods
 
         // Draw menu
+        /// <summary>
+        /// Draws the menu from the string array that was provided in the constructor or set as a property.
+        /// </summary>
         public void DrawMenu()
         {
             // Check that the array contains items 
@@ -72,6 +98,10 @@ namespace ConsoleMenu
         }
 
         // Receive input (menu item number), returns to caller
+        /// <summary>
+        /// Reads the users input of what menu choice they want. [Currently does not implement validation or error handling]
+        /// </summary>
+        /// <returns>Returns an int of what menu item was chosen</returns>
         public int ReadInput()
         {
             // The most basic of input... this is mostly just so I can start testing; should change this to be more robust and validate input
